4c079a22025249754918f824f279a3261030a995
kroczniok <kroczniok@gmail.com>
2025-11-08 18:56:05 +0100

    Predict games from backlog, the owner should try to play

--- gamerating_hltb.py
+++ gamerating_hltb.py
@@ -25,6 +25,7 @@ df["old_game"] = pd.to_numeric(df["old_game"], errors="coerce").astype("Int64") 
 
 df = df[~(df[cols] == 0).any(axis=1)] #remove a row when value is missing
 df = df[df["game_type"] == "game"] #train only with game type, because sport/endless etc are not relevant
+df = df[df["list_replay"] == 0] #train only with game type, because sport/endless etc are not relevant
 
 df["comp_all"] = pd.to_numeric(df["comp_all"], errors="coerce")
 df["list_comp"] = pd.to_numeric(df["list_comp"], errors="coerce").astype("Int64")
@@ -49,7 +50,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 #Initialise the Support Vector Classifier
-model = SVC(kernel='rbf', class_weight='balanced', probability=True, random_state=42)
+model = SVC(kernel='rbf', probability=True, random_state=42)
 
 #Train the model using the training data
 model.fit(X_train_scaled, y_train)
@@ -73,13 +74,13 @@ predict = model.predict(test_scaled)
 
 print(f"\nAn old game ~20 hours and 75 rating:", predict)
 
-test_object = [[108000, 65, 0]]
+test_object = [[90000, 78, 0]]
 test_scaled = scaler.transform(test_object)          # <- scale before predict
 predict = model.predict(test_scaled)
 
-print(f"\nA game ~30 hours and 75 rating:", predict)
+print(f"\nA new game ~30 hours and 75 rating:", predict)
 
-test_object = [[75000, 90, 0]]
+test_object = [[100000, 88, 0]]
 test_scaled = scaler.transform(test_object)          # <- scale before predict
 predict = model.predict(test_scaled)
 
@@ -90,3 +91,23 @@ test_scaled = scaler.transform(test_object)          # <- scale before predict
 predict = model.predict(test_scaled)
 
 print(f"\nAn old game ~30 hours and 65 rating:", predict)
+
+# get top 10 games from my backlog I may probably complete
+games = []
+for item in data['lines']:
+    release_year = pd.to_datetime(item["release_world"], errors="coerce")
+    is_old_game_int = int (release_year.year < 2016)
+
+    # is_old_game_int = 1
+
+    if item['game_type'] == "game" and item['list_comp'] == 0 and item['list_replay'] == 0:
+        test_object = [[item["comp_all"], item["review_score_g"], is_old_game_int]]
+        test_scaled = scaler.transform(test_object)  # <- scale before predict
+        predict = model.predict(test_scaled)
+        if predict == 1:
+            games.append(item)
+
+games.sort(key=lambda g: g["review_score_g"], reverse=True)
+for item in games:
+    print(f'You should try to play this game:', item['custom_title'], ' rating: ', item['review_score_g'],
+          ' time to beat: ~', item['comp_all'] / 3600, 'hours')
